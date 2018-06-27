a9db138a08d5477a76c33051ee6f298937abece4
jono659 <jono@gr8hosts.com>
2018-06-27 10:54:20 +0100

    Pirates and Docks Lua

--- scripts/zones/Mhaura/npcs/Yabby_Tanmikey.lua
+++ scripts/zones/Mhaura/npcs/Yabby_Tanmikey.lua
@@ -15,33 +15,13 @@ function onTrade(player,npc,trade)
 end;
 
 function onTrigger(player,npc)
-player:showText(npc,MATOAKA_SHOP_DIALOG);
-
-stock = {640,225, --Copper Ore
-		 736,375, --Silver Ore
-		 644,500, --Mythril Ore
-		 771,250, --Yellow Rock
-		 774,250, --Purple Rock
-		 770,250, --Blue Rock
-		 772,250, --Green Rock
-		 773,250, --Translucent rock
-		 775,250, --Black Rock
-		 776,250, --White Rock
-		 769,250, -- Red Rock
-		 4484,300, --Shall Shell		 
-		 790,25000, -- Garnet
-         } -- doesnt work
-
- 
-showShop(player, STATIC, stock);
+    if (player:sendGuild(528,8,23,4)) then
+        player:showText(npc,GOLDSMITHING_GUILD);
+    end
 end;
 
 function onEventUpdate(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
 end;
 
 function onEventFinish(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
 end;
